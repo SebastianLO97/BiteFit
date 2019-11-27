d5ebb5de48ca7909ae58637673fe22ad3f9ba058
Sebastian Lopez <jsebastian.2008@hotmail.com>
2019-11-27 16:05:01 -0600

    Register Backend

---
+++ BiteFit/Controllers/RegisterUserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BiteFit.Data;
+using BiteFit.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BiteFit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterUserController : ControllerBase
+    {
+        [HttpGet("{idGet}")]
+        public dynamic GetCRegisterUser([FromRoute]string idGet)
+        {
+            switch (idGet)
+            {
+                case "Pais":
+                    var paisDb = new PaisDb();
+                    return paisDb.GetPaises();
+                case "Estado":
+                    var estadoDb = new EstadoDb();
+                    return estadoDb.GetEstados();
+                default: return null;
+            }
+            
+        }
+    }
+}
